47cde85c2eb213556fee78985f171103fcb5bae2
Anish Roy <62830866+Anish6275@users.noreply.github.com>
2021-08-21 17:45:27 +0530

    home

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "contexts/auth";
 import { useHistory } from "react-router-dom";
+import { useSnackbar } from "react-simple-snackbar";
 import startLinkGame from "functions/startGame";
+import "./style.scss";
+
 const Home = () => {
   const history = useHistory();
+  const [openSnackbar] = useSnackbar();
+  const [btnText, setBtnText] = useState("Start Game");
   const { currentUser, signOut } = useAuth();
   useEffect(() => {
     let link_matchId = sessionStorage.getItem("link-matchId");
@@ -13,27 +18,42 @@ const Home = () => {
     }
   }, [history]);
   const startGameWithLink = () => {
+    setBtnText("Creating Room...");
     startLinkGame(currentUser)
       .then((matchId) => {
         if (matchId) {
+          openSnackbar("Room Created Successfully!!");
           history.push("/play/" + matchId);
         } else {
-          console.log("error");
+          openSnackbar("Some Error Occurred!!");
         }
+        setBtnText("Start Game");
       })
       .catch(() => {
-        console.log("err");
+        openSnackbar("Some Error Occurred!!");
+        setBtnText("Start Game");
       });
   };
   return (
     <>
-      <h1>Home</h1>
-      <button onClick={signOut}>signOut</button>
-
-      <br />
-      <br />
-
-      <button onClick={startGameWithLink}>start Game</button>
+      <div className="home">
+        <div className="profile">
+          <div className="left">
+            <img src={currentUser.photoURL} alt={currentUser.displayName} />
+          </div>
+          <div className="right">
+            <h2>
+              {currentUser.displayName} <span onClick={signOut}>🚪</span>
+            </h2>
+            <h3>{currentUser.email}</h3>
+            <p>12 matches played</p>
+            <p>12 matches won</p>
+          </div>
+        </div>
+        <div className="cta">
+          <button onClick={startGameWithLink}>{btnText}</button>
+        </div>
+      </div>
     </>
   );
 };
